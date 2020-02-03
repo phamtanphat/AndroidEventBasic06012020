5c568e33f748c7b4a01de7b14437c0970e324a5f
phatdroid94 <phatdroid94@gmail.com>
2020-02-03 20:23:19 +0700

    init

---
+++ app/src/main/java/com/example/androideventbasic06012020/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.androideventbasic06012020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // 1 : Task : Bắt sự kiện cho view
+        // 2 : Task : Kiểm tra dữ liệu theo log
+        // 3 : Task : Hiển thị dư liệu ra màn hình
+    }
+}
